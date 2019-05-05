1e004c012a2f13bdda499a10a6b87dad44c204ae
thiagodsd <thiagosd.2.18@gmail.com>
2019-05-05 01:56:38 -0300

    final changes

--- quiz.js
+++ quiz.js
@@ -13,7 +13,9 @@
   // QUESTIONS
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   const myQuestions = [
-    {
+
+
+{
       question: "Você poupa?",
       answers: {
 
@@ -34,7 +36,8 @@
                     [0.3, 0.3, 0.3, 0.2, 0.1],
 
                     [0.6, 0.2, 0.1, 0.0, 0.0] ],
-      img: "pig"
+      img: "saving",
+      button: "r"
     },
 
 {
@@ -45,7 +48,9 @@
 
         1:"Realizar sonhos",
 
-        2:"Previdência privada" },
+        2:"Previdência privada",
+
+        3:"Não poupo!" },
 
       weights: [
 
@@ -53,8 +58,11 @@
 
                     [0.1, 0.1, 0.3, 0.4, 0.5],
 
-                    [0.0, 0.0, 0.1, 0.2, 0.3] ],
-      img: "pig"
+                    [0.0, 0.0, 0.1, 0.2, 0.3],
+
+                    [0.8, 0.4, 0.15, 0.1, 0.05] ],
+      img: "saving2",
+      button: "c"
     },
 
 {
@@ -74,7 +82,33 @@
                     [0.2, 0.3, 0.3, 0.3, 0.4],
 
                     [0.4, 0.3, 0.5, 0.2, 0.0] ],
-      img: "pig"
+      img: "saving3",
+      button: "r"
+    },
+
+{
+      question: "Suas reservas cobrem quantos meses das suas despesas?",
+      answers: {
+
+        0:"12 meses ou mais",
+
+        1:"mais ou menos 6 meses",
+
+        2:"mais ou menos 3 meses",
+
+        3:"mais ou menos 1 mês" },
+
+      weights: [
+
+                    [0.0, 0.1, 0.2, 0.4, 0.8],
+
+                    [0.1, 0.2, 0.1, 0.35, 0.3],
+
+                    [0.3, 0.3, 0.15, 0.25, 0.1],
+
+                    [0.6, 0.4, 0.3, 0.0, 0.0] ],
+      img: "shield",
+      button: "r"
     },
 
 {
@@ -102,7 +136,8 @@
                     [0.05, 0.2, 0.3, 0.35, 0.1],
 
                     [0.2, 0.3, 0.35, 0.1, 0.05] ],
-      img: "pig"
+      img: "card",
+      button: "c"
     },
 
 {
@@ -122,7 +157,8 @@
                     [0.2, 0.5, 0.4, 0.3, 0.2],
 
                     [0.8, 0.3, 0.3, 0.2, 0.0] ],
-      img: "pig"
+      img: "idea",
+      button: "r"
     },
 
 {
@@ -150,7 +186,8 @@
                     [0.4, 0.3, 0.25, 0.15, 0.05],
 
                     [0.6, 0.5, 0.2, 0.0, 0.0] ],
-      img: "pig"
+      img: "balance",
+      button: "r"
     },
 
 {
@@ -174,7 +211,8 @@
                     [0.3, 0.3, 0.3, 0.2, 0.1],
 
                     [0.6, 0.2, 0.1, 0.0, 0.0] ],
-      img: "pig"
+      img: "bill",
+      button: "r"
     },
 
 {
@@ -198,7 +236,8 @@
                     [0.3, 0.3, 0.3, 0.2, 0.1],
 
                     [0.6, 0.2, 0.1, 0.0, 0.0] ],
-      img: "pig"
+      img: "calendar",
+      button: "r"
     },
 
 {
@@ -222,7 +261,8 @@
                     [0.3, 0.3, 0.3, 0.2, 0.1],
 
                     [0.6, 0.4, 0.2, 0.0, 0.0] ],
-      img: "pig"
+      img: "balance2",
+      button: "r"
     },
 
 {
@@ -246,7 +286,8 @@
                     [0.0, 0.0, 0.1, 0.3, 0.4],
 
                     [0.1, 0.1, 0.2, 0.4, 0.6] ],
-      img: "pig"
+      img: "insurance",
+      button: "r"
     },
 
 {
@@ -262,7 +303,8 @@
                     [0.8, 0.7, 0.3, 0.2, 0.1],
 
                     [0.2, 0.3, 0.7, 0.8, 0.9] ],
-      img: "pig"
+      img: "management",
+      button: "r"
     },
 
 {
@@ -286,7 +328,8 @@
                     [0.1, 0.2, 0.3, 0.5, 0.4],
 
                     [0.8, 0.5, 0.2, 0.1, 0.0] ],
-      img: "pig"
+      img: "analytics",
+      button: "c"
     },
 
 {
@@ -310,7 +353,8 @@
                     [0.4, 0.4, 0.3, 0.2, 0.0],
 
                     [0.5, 0.1, 0.0, 0.0, 0.0] ],
-      img: "pig"
+      img: "debt",
+      button: "r"
     }
     
   ];
@@ -323,12 +367,23 @@
 
     myQuestions.forEach((currentQuestion, questionNumber) => {
       const answers = [];
+      var button  = "radio";
+      var bclass  = "form-radio";
+
+      if (currentQuestion.button == "r") {
+      		button = "radio";
+      		bclass = "form-radio";
+      }
+      else {
+      		button = "checkbox";
+      		bclass = "form-check";
+      }
 
       for (letter in currentQuestion.answers) {
         answers.push(
           `<div style="display: table;">
              <div style="display: table-row;">
-              <div style="display: table-cell;"><input type="radio" name="question${questionNumber}" value="${letter}" class="form-radio"></div>
+              <div style="display: table-cell;"><input type="${button}" name="question${questionNumber}" value="${letter}" class="${bclass}"></div>
               <div style="display: table-cell;"><p>${currentQuestion.answers[letter]}</p></div>
              </div>
            </div>
@@ -339,7 +394,7 @@
       output.push(
         `<section>
            <div class="question"> <h1> ${currentQuestion.question} </h1> </div>
-           <img style="float: right; margin-right: 3em; width: 450px;" alt="" src="img/${currentQuestion.img}.png" />
+           <img style="float: right; margin-right: 3em; height: 10%;" alt="" src="img/${currentQuestion.img}.png" />
            <div class="answers"> ${answers.join("")} </div>
          </section>`
       );
@@ -369,12 +424,33 @@
     // for question
     myQuestions.forEach((currentQuestion, questionNumber) => {
       // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector        = `input[name=question${questionNumber}]:checked`;
-      const userAnswer      = (answerContainer.querySelector(selector) || {}).value;
-      const score           = currentQuestion.weights[userAnswer]
+      //const answerContainer = answerContainers[questionNumber];
+      //const selector        = `input[name=question${questionNumber}]:checked`;
+      //const userAnswer      = (answerContainer.querySelector(selector) || {}).value;
+      //const score           = currentQuestion.weights[userAnswer];
+      const tmpScore = [];
+      const tmpProf  = [0.0, 0.0, 0.0, 0.0, 0.0];
+      const checkboxes = document.querySelectorAll(`input[name=question${questionNumber}]`);
       
-      scoreboard.splice( questionNumber, 0, score );
+      for (var i=0; i<checkboxes.length; i++ ) {
+      		if (checkboxes[i].checked) {
+      			const val   = checkboxes[i].value;
+      			const score = currentQuestion.weights[val];
+      			tmpScore.splice( questionNumber, 0, score );
+      		}
+      }
+
+      for ( const i of Array(5).keys() ) {
+	      var SUM = 0;
+	      
+	      for (var index = 0; index < tmpScore.length; index++) {
+	        SUM += tmpScore[index][i] ;
+	      };
+	      
+	      tmpProf.splice(i, 1, SUM);
+   	  };
+      
+      scoreboard.splice( questionNumber, 0, tmpProf );
     });
     
     for ( const i of Array(5).keys() ) {
@@ -393,14 +469,14 @@
     console.log(profile);
     console.log(persona);
 
-    resultsContainer.innerHTML = `Persona: ${persona[0]} <br/><br/>
+    resultsContainer.innerHTML = `Persona: ${Number(persona[0].toFixed(2))} <br/><br/>
                                   Scores:<br/>
-                                          0:  ${profile[0]},<br/>
-                                          1:  ${profile[1]},<br/>
-                                          2:  ${profile[2]},<br/>
-                                          3:  ${profile[3]},<br/>
-                                          4:  ${profile[4]} <br/></br/><br/>
-                                  FECHAR (falta implementar)`;
+                                          0:  ${Number(profile[0].toFixed(2))},<br/>
+                                          1:  ${Number(profile[1].toFixed(2))},<br/>
+                                          2:  ${Number(profile[2].toFixed(2))},<br/>
+                                          3:  ${Number(profile[3].toFixed(2))},<br/>
+                                          4:  ${Number(profile[4].toFixed(2))} <br/></br/><br/>
+                                  `;
   }
   
   const quizContainer    = document.getElementById("quiz");
