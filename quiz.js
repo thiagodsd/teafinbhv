aa078b20e6c2f5915f1011d369ec8a798d786209
thiagodsd <thiagosd.2.18@gmail.com>
2019-05-07 23:05:39 -0300

    final version

--- quiz.js
+++ quiz.js
@@ -16,51 +16,55 @@
 
 
 {
-      question: "Você poupa?",
+      question: "Você costuma guardar uma graninha?",
       answers: {
 
-        0:"Sempre",
+        0:"Sim, sempre guardo uma parte, com disciplina.",
 
-        1:"Às vezes",
+        1:"Geralmente, procuro manter a regularidade, mas as vezes dou uma pedalada.",
 
-        2:"Raramente",
+        2:"Eu tento, mas confesso que não é um hábito.",
 
-        3:"Nunca" },
+        3:"Nunca sobra nada do meu salário para eu guardar!" },
 
       weights: [
 
-                    [0.0, 0.1, 0.2, 0.5, 0.7],
+                    [0.0, 0.1, 0.3, 0.4, 0.6],
 
-                    [0.1, 0.4, 0.4, 0.3, 0.2],
+                    [0.1, 0.2, 0.4, 0.5, 0.3],
 
-                    [0.3, 0.3, 0.3, 0.2, 0.1],
+                    [0.1, 0.5, 0.4, 0.3, 0.1],
 
-                    [0.6, 0.2, 0.1, 0.0, 0.0] ],
+                    [0.1, 0.4, 0.2, 0.0, 0.0] ],
       img: "saving",
       button: "r"
     },
 
 {
-      question: "Para que poupa?",
+      question: "Se você tem um dinheirinho guardado, o que planeja para ele?",
       answers: {
 
-        0:"Reserva de emergencia",
+        0:"Planejo usar em uma emergência, nunca se sabe o que está por vir.",
 
-        1:"Realizar sonhos",
+        1:"Estou juntando para um sonho que quero realizar.",
 
-        2:"Previdência privada",
+        2:"Penso no futuro e guardo para minha aposentadoria.",
 
-        3:"Não poupo!" },
+        3:"Estou só guardando, mas ainda não planejei nada específico.",
+
+        4:"Não tenho dinheiro guardado." },
 
       weights: [
 
-                    [0.0, 0.0, 0.1, 0.1, 0.2],
+                    [0.0, 0.0, 0.1, 0.2, 0.5],
 
-                    [0.1, 0.1, 0.3, 0.4, 0.5],
+                    [0.1, 0.2, 0.4, 0.7, 0.3],
 
-                    [0.0, 0.0, 0.1, 0.2, 0.3],
+                    [0.0, 0.0, 0.1, 0.4, 0.4],
 
-                    [0.8, 0.4, 0.15, 0.1, 0.05] ],
+                    [0.1, 0.2, 0.3, 0.2, 0.1],
+
+                    [0.4, 0.3, 0.1, 0.0, 0.0] ],
       img: "saving2",
       button: "c"
     },
@@ -69,17 +73,21 @@
       question: "Você poupa o máximo que pode?",
       answers: {
 
-        0:"Poupo o máximo possível",
+        0:"Sim, já fiz as contas e poupo o máximo possível.",
 
-        1:"Poderia guardar mais",
+        1:"Acho que poderia guardar um pouco mais.",
 
-        2:"Poderia começar a guardar mais vezes" },
+        2:"Penso em adquirir esse hábito.",
+
+        3:"Poxa, realmente não consigo poupar!" },
 
       weights: [
 
-                    [0.4, 0.4, 0.2, 0.5, 0.6],
+                    [0.4, 0.4, 0.2, 0.3, 0.6],
 
-                    [0.2, 0.3, 0.3, 0.3, 0.4],
+                    [0.1, 0.2, 0.3, 0.5, 0.3],
+
+                    [0.2, 0.3, 0.5, 0.4, 0.2],
 
                     [0.4, 0.3, 0.5, 0.2, 0.0] ],
       img: "saving3",
@@ -87,106 +95,56 @@
     },
 
 {
-      question: "Suas reservas cobrem quantos meses das suas despesas?",
-      answers: {
-
-        0:"12 meses ou mais",
-
-        1:"mais ou menos 6 meses",
-
-        2:"mais ou menos 3 meses",
-
-        3:"mais ou menos 1 mês" },
-
-      weights: [
-
-                    [0.0, 0.1, 0.2, 0.4, 0.8],
-
-                    [0.1, 0.2, 0.1, 0.35, 0.3],
-
-                    [0.3, 0.3, 0.15, 0.25, 0.1],
-
-                    [0.6, 0.4, 0.3, 0.0, 0.0] ],
-      img: "shield",
-      button: "r"
-    },
-
-{
-      question: "O que você usa do banco?",
+      question: "Atualmente, quais dos produtos abaixo você usa?",
       answers: {
 
         0:"Cheque especial",
 
-        1:"Cartão",
+        1:"Cartão de crédito",
 
         2:"Empréstimo",
 
         3:"Consórcio",
 
-        4:"Financiamento" },
+        4:"Financiamento",
+
+        5:"Não uso nenhum desses." },
 
       weights: [
 
                     [0.4, 0.3, 0.2, 0.1, 0.0],
 
-                    [0.1, 0.08, 0.02, 0.08, 0.1],
+                    [0.1, 0.2, 0.02, 0.3, 0.2],
 
                     [0.3, 0.2, 0.1, 0.05, 0.05],
 
-                    [0.05, 0.2, 0.3, 0.35, 0.1],
+                    [0.05, 0.2, 0.3, 0.6, 0.1],
 
-                    [0.2, 0.3, 0.35, 0.1, 0.05] ],
+                    [0.2, 0.3, 0.35, 0.1, 0.05],
+
+                    [0.0, 0.0, 0.0, 0.0, 0.0] ],
       img: "card",
       button: "c"
     },
 
 {
-      question: "Você entende os produtos que usa?",
+      question: "Sobre os produtos da pergunta anterior, quão bem você os entende?",
       answers: {
 
-        0:"Entendo exatamente",
+        0:"Entendo bem, sei exatamente como e quando usá-los corretamente.",
 
-        1:"Mais ou menos",
+        1:"Entendo bem alguns deles, mas outros são um verdadeiro mistério.",
 
-        2:"Não entendo direito" },
+        2:"Não entendo direito, mas acabo usando conforme a necessidade." },
 
       weights: [
 
-                    [0.0, 0.2, 0.3, 0.5, 0.8],
+                    [0.0, 0.2, 0.3, 0.4, 0.6],
 
                     [0.2, 0.5, 0.4, 0.3, 0.2],
 
-                    [0.8, 0.3, 0.3, 0.2, 0.0] ],
+                    [0.4, 0.5, 0.3, 0.2, 0.0] ],
       img: "idea",
-      button: "r"
-    },
-
-{
-      question: "Os produtos que você usa representam quanto do seu salário?",
-      answers: {
-
-        0:"0%",
-
-        1:"25%",
-
-        2:"50%",
-
-        3:"75%",
-
-        4:"100%" },
-
-      weights: [
-
-                    [0.0, 0.0, 0.1, 0.3, 0.6],
-
-                    [0.0, 0.1, 0.2, 0.4, 0.5],
-
-                    [0.25, 0.2, 0.2, 0.15, 0.15],
-
-                    [0.4, 0.3, 0.25, 0.15, 0.05],
-
-                    [0.6, 0.5, 0.2, 0.0, 0.0] ],
-      img: "balance",
       button: "r"
     },
 
@@ -194,21 +152,21 @@
       question: "Você paga suas contas em dia?",
       answers: {
 
-        0:"Sempre",
+        0:"Sempre pago, não gosto de atrasar as contas!",
 
-        1:"Quase sempre",
+        1:"Quase sempre, as vezes esqueço de alguma.",
 
-        2:"Raramente",
+        2:"Raramente, eu sempre esqueço, mas quando lembro a tempo, corro e pago.",
 
-        3:"Nunca" },
+        3:"Nunca, sempre me perco com as datas de vencimento das contas!" },
 
       weights: [
 
                     [0.0, 0.1, 0.2, 0.5, 0.7],
 
-                    [0.1, 0.4, 0.4, 0.3, 0.2],
+                    [0.1, 0.4, 0.5, 0.4, 0.2],
 
-                    [0.3, 0.3, 0.3, 0.2, 0.1],
+                    [0.3, 0.5, 0.3, 0.2, 0.1],
 
                     [0.6, 0.2, 0.1, 0.0, 0.0] ],
       img: "bill",
@@ -216,26 +174,26 @@
     },
 
 {
-      question: "Quanto tempo você demoraria para pagar suas parcelas?",
+      question: "Quanto tempo você demoraria para pagar todos os seus parcelamentos atuais?",
       answers: {
 
-        0:"1 mês",
+        0:"Entre 1mês e 6 meses..",
 
-        1:"6 meses",
+        1:"Entre 6 meses e 1 ano.",
 
-        2:"1 ano",
+        2:"Entre 1 e 5 anos.",
 
-        3:"5 anos ou mais" },
+        3:"Mais de 5 anos." },
 
       weights: [
 
                     [0.0, 0.1, 0.2, 0.5, 0.7],
 
-                    [0.1, 0.4, 0.4, 0.3, 0.2],
+                    [0.1, 0.4, 0.4, 0.7, 0.2],
 
-                    [0.3, 0.3, 0.3, 0.2, 0.1],
+                    [0.3, 0.5, 0.3, 0.2, 0.1],
 
-                    [0.6, 0.2, 0.1, 0.0, 0.0] ],
+                    [0.7, 0.2, 0.1, 0.0, 0.0] ],
       img: "calendar",
       button: "r"
     },
@@ -244,13 +202,13 @@
       question: "Suas contas fixas represetam quanto do seu salário?",
       answers: {
 
-        0:"50% ou menos",
+        0:"25% ou menos.",
 
-        1:"70%",
+        1:"Entre 25% e 50%.",
 
-        2:"90%",
+        2:"Entre 50% e 75%.",
 
-        3:"100%" },
+        3:"Mais de 75%." },
 
       weights: [
 
@@ -258,15 +216,15 @@
 
                     [0.1, 0.3, 0.3, 0.5, 0.3],
 
-                    [0.3, 0.3, 0.3, 0.2, 0.1],
+                    [0.3, 0.4, 0.6, 0.4, 0.1],
 
-                    [0.6, 0.4, 0.2, 0.0, 0.0] ],
+                    [0.7, 0.6, 0.3, 0.1, 0.0] ],
       img: "balance2",
       button: "r"
     },
 
 {
-      question: "Você tem algum seguro?",
+      question: "Você tem algum tipo de seguro contratado atualmente?",
       answers: {
 
         0:"Imóvel",
@@ -275,17 +233,21 @@
 
         2:"Vida",
 
-        3:"Convênio médico" },
+        3:"Convênio médico",
+
+        4:"Não tenho." },
 
       weights: [
 
-                    [0.0, 0.0, 0.1, 0.3, 0.3],
+                    [0.0, 0.0, 0.1, 0.3, 0.6],
 
-                    [0.0, 0.1, 0.1, 0.4, 0.4],
+                    [0.0, 0.1, 0.1, 0.4, 0.5],
 
-                    [0.0, 0.0, 0.1, 0.3, 0.4],
+                    [0.0, 0.0, 0.1, 0.3, 0.5],
 
-                    [0.1, 0.1, 0.2, 0.4, 0.6] ],
+                    [0.1, 0.1, 0.5, 0.4, 0.6],
+
+                    [0.7, 0.7, 0.4, 0.3, 0.0] ],
       img: "insurance",
       button: "r"
     },
@@ -294,15 +256,15 @@
       question: "Você gasta mais do que ganha?",
       answers: {
 
-        0:"Sim",
+        0:"Sim, um aumentinho de salário cairia bem…",
 
-        1:"Não" },
+        1:"Não, consigo manter a disciplina." },
 
       weights: [
 
                     [0.8, 0.7, 0.3, 0.2, 0.1],
 
-                    [0.2, 0.3, 0.7, 0.8, 0.9] ],
+                    [0.2, 0.3, 0.7, 0.4, 0.4] ],
       img: "management",
       button: "r"
     },
@@ -311,21 +273,21 @@
       question: "Como você organiza suas finanças?",
       answers: {
 
-        0:"Com aplicativos",
+        0:"Gosto de tecnologia, uso aplicativos e/ou ferramentas feitas pra isso!",
 
-        1:"Planilhas no computador",
+        1:"Minha querida planilha me ajuda com isso.",
 
-        2:"Anotações",
+        2:"Sou old school, caneta, papel e calculadora é tudo que eu preciso.",
 
-        3:"Não organizo" },
+        3:"Não costumo organizar de jeito nenhum." },
 
       weights: [
 
-                    [0.0, 0.1, 0.4, 0.5, 0.6],
+                    [0.0, 0.1, 0.4, 0.6, 0.5],
 
-                    [0.1, 0.2, 0.3, 0.5, 0.7],
+                    [0.1, 0.2, 0.3, 0.6, 0.5],
 
-                    [0.1, 0.2, 0.3, 0.5, 0.4],
+                    [0.1, 0.2, 0.3, 0.6, 0.5],
 
                     [0.8, 0.5, 0.2, 0.1, 0.0] ],
       img: "analytics",
@@ -336,24 +298,49 @@
       question: "Você tem muitas dívidas?",
       answers: {
 
-        0:"Não tenho",
+        0:"Digo com orgulho: não!",
 
-        1:"Sim, mas estão sob controle",
+        1:"Tenho, mas está tudo sob controle, sei o que estou fazendo.",
 
-        2:"Sim e estão me complicando",
+        2:"Sim, tenho e sinceramente estou perdendo o controle.",
 
-        3:"Sim e não sei o que fazer!" },
+        3:"Tenho e já nem sei o que está acontecendo, acho que preciso de ajuda!" },
 
       weights: [
 
                     [0.1, 0.3, 0.5, 0.6, 0.7],
 
-                    [0.4, 0.6, 0.5, 0.4, 0.3],
+                    [0.4, 0.6, 0.6, 0.6, 0.7],
 
                     [0.4, 0.4, 0.3, 0.2, 0.0],
 
                     [0.5, 0.1, 0.0, 0.0, 0.0] ],
       img: "debt",
+      button: "r"
+    },
+
+{
+      question: "Se você parasse de trabalhar hoje, por quanto tempo conseguiria viver no seu padrão de vida atual?",
+      answers: {
+
+        0:"Mais ou menos 1 ano, o suficiente para encontrar um novo emprego.",
+
+        1:"Conseguiria me manter por uns 6 meses, não dá pra vacilar.",
+
+        2:"Viveria do jeito que vivo agora por uns 3 meses, depois teria que apertar o cinto.",
+
+        3:"Me manteria apenas por 1 mês, teria que achar um novo emprego bem rápido!" },
+
+      weights: [
+
+                    [0.0, 0.1, 0.3, 0.4, 0.7],
+
+                    [0.0, 0.2, 0.5, 0.7, 0.3],
+
+                    [0.1, 0.6, 0.7, 0.4, 0.2],
+
+                    [0.6, 0.6, 0.4, 0.1, 0.0] ],
+      img: "shield",
       button: "r"
     }
     
@@ -394,7 +381,7 @@
       output.push(
         `<section>
            <div class="question"> <h1> ${currentQuestion.question} </h1> </div>
-           <img style="float: right; margin-right: 3em; height: 10%;" alt="" src="img/${currentQuestion.img}.png" />
+           <img style="float: right; margin-left: 3em; height: 10%;" alt="" src="img/${currentQuestion.img}.png" />
            <div class="answers"> ${answers.join("")} </div>
          </section>`
       );
